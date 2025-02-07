9ff361d4e95fefbf7bcc046ef7cecb3711ab24f5
Marcel Steyrer <marcelsteyrer@gmail.com>
2025-02-07 15:06:16 +0100

    session creation logic

--- MotionDrive.Recorder/iRacingRecorder/iRacing.cs
+++ MotionDrive.Recorder/iRacingRecorder/iRacing.cs
@@ -1,11 +1,16 @@
 ﻿using MotionDrive.Recorder.ACCRecorder;
 using MotionDrive.Recorder.ACCRecorder.SharedMemory;
+using MotionDrive.Recorder.ACCRecorder.SharedMemory.Models;
+using MotionDrive.Recorder.Enum;
+using MotionDrive.Recorder.iRacingRecorder.irsdksharp;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YamlDotNet.Serialization;
 using static MotionDrive.Recorder.iRacingRecorder.SdkWrapper;
 
 namespace MotionDrive.Recorder.iRacingRecorder;
@@ -15,10 +20,13 @@ internal class iRacing : IGameRecorder
     SdkWrapper wrapper = new SdkWrapper();
     public void Read()
     {
+        this.wrapper.SessionInfoUpdated += this.OnSessionInfoUpdated;
         this.wrapper.TelemetryUpdated += this.OnTelemetryUpdated;
 
         this.wrapper.Start();
     }
+
+
     public void StopReading()
     {
         this.wrapper.Stop();
@@ -55,8 +63,40 @@ internal class iRacing : IGameRecorder
         Trace.WriteLine("iRacing STOPPED");
     }
 
+    public int? CurrentSessionIndex;
+    public double LastUpdateTime = 0;
+    
+
+    private void OnSessionInfoUpdated(object sender, SessionInfoUpdatedEventArgs e)
+    {
+        // e.UpdateTime => check if data is not coming twice
+        // Trace.WriteLine(e.SessionInfo);
+
+        var deserializer = new Deserializer();
+        Dictionary<object, object> result = deserializer.Deserialize<Dictionary<object, object>>(new StringReader(e.SessionInfo));
+
+        var weekendInfo = result["WeekendInfo"] as Dictionary<object, object>;
+        var driverInfo = result["DriverInfo"] as Dictionary<object, object>;
+
+        int driverIdx = int.Parse(driverInfo["DriverCarIdx"].ToString());
+        Dictionary<object, object> driver = ((List<object>)driverInfo["Drivers"])[driverIdx] as Dictionary<object, object>;
+
+        rm.NewSessionStarted(driver["CarScreenName"] as string, weekendInfo["TrackName"] as string, SessionType.PRACTICE, Game.IRACING);
+    }
+
     private void OnTelemetryUpdated(object sender, TelemetryUpdatedEventArgs e)
     {
-        Trace.WriteLine(e.TelemetryInfo.Throttle);   
+        //e.TelemetryInfo.SessionNum;
+
+        if (LastUpdateTime == e.UpdateTime)
+            return;
+
+        if (CurrentSessionIndex == null || e.TelemetryInfo.SessionNum.Value != CurrentSessionIndex )
+        {
+            CurrentSessionIndex = e.TelemetryInfo.SessionNum.Value;
+            SessionType st;
+
+        }
     }
+
 }
